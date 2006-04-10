a51d8862a4262949e9809e7c781fc74b508fa6d8
huangwei <huangwei@09bc9535-d30e-0410-b1f7-d46b20a4725c>
2006-04-10 20:15:20 +0000

    new copyright info for osu benchmarks.
    git-svn-id: https://scm.mvapich.cse.ohio-state.edu/svn/mpi/mvapich2/trunk@52 09bc9535-d30e-0410-b1f7-d46b20a4725c

--- osu_benchmarks/osu_latency.c
+++ osu_benchmarks/osu_latency.c
@@ -1,40 +1,79 @@
+
 /*
- * Copyright (C) 2002-2005 the Network-Based Computing Laboratory
- * (NBCL), The Ohio State University.  
+ * OSU MPI Latency test v2.2
+ */
+/*
+ * Copyright (C) 2002-2006 the Network-Based Computing Laboratory
+ * (NBCL), The Ohio State University. 
+ *
+ * Contact: Dr. D. K. Panda (panda@cse.ohio-state.edu)
  */
 
+/*
+This program is available under BSD licensing.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+(1) Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+
+(2) Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+
+(3) Neither the name of The Ohio State University nor the names of
+their contributors may be used to endorse or promote products derived
+from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+#include "mpi.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <unistd.h>
 
-#include "mpi.h"
 #define MESSAGE_ALIGNMENT 64
 #define MAX_MSG_SIZE (1<<22)
 #define MYBUFSIZE (MAX_MSG_SIZE + MESSAGE_ALIGNMENT)
 
-char        s_buf_original[MYBUFSIZE];
-char        r_buf_original[MYBUFSIZE];
+char s_buf_original[MYBUFSIZE];
+char r_buf_original[MYBUFSIZE];
 
-int         skip = 1000;
-int         loop = 10000;
-int         skip_large = 10;
-int         loop_large = 100;
-int         large_message_size = 8192;
+int skip = 1000;
+int loop = 10000;
+int skip_large = 10;
+int loop_large = 100;
+int large_message_size = 8192;
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-    int         myid, numprocs, i;
-    int         size;
-    MPI_Status  reqstat;
-    char       *s_buf, *r_buf;
-    int         align_size;
 
-    double      t_start = 0.0, t_end = 0.0;
+    int myid, numprocs, i;
+    int size;
+    MPI_Status reqstat;
+    char *s_buf, *r_buf;
+    int align_size;
 
-    MPI_Init (&argc, &argv);
-    MPI_Comm_size (MPI_COMM_WORLD, &numprocs);
-    MPI_Comm_rank (MPI_COMM_WORLD, &myid);
+    double t_start = 0.0, t_end = 0.0;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myid);
 
     align_size = MESSAGE_ALIGNMENT;
 
@@ -46,8 +85,8 @@ int main (int argc, char *argv[])
                   align_size * align_size);
 
     if (myid == 0) {
-        fprintf (stdout, "# OSU MPI Latency Test (Version 2.1)\n");
-        fprintf (stdout, "# Size\t\tLatency (us) \n");
+        fprintf(stdout, "# OSU MPI Latency Test (Version 2.2)\n");
+        fprintf(stdout, "# Size\t\tLatency (us) \n");
     }
 
     for (size = 0; size <= MAX_MSG_SIZE;
@@ -64,33 +103,35 @@ int main (int argc, char *argv[])
             skip = skip_large;
         }
 
-        MPI_Barrier (MPI_COMM_WORLD);
+        MPI_Barrier(MPI_COMM_WORLD);
 
         if (myid == 0) {
             for (i = 0; i < loop + skip; i++) {
                 if (i == skip)
-                    t_start = MPI_Wtime ();
-                MPI_Send (s_buf, size, MPI_CHAR, 1, 1, MPI_COMM_WORLD);
-                MPI_Recv (r_buf, size, MPI_CHAR, 1, 1, MPI_COMM_WORLD,
-                          &reqstat);
+                    t_start = MPI_Wtime();
+                MPI_Send(s_buf, size, MPI_CHAR, 1, 1, MPI_COMM_WORLD);
+                MPI_Recv(r_buf, size, MPI_CHAR, 1, 1, MPI_COMM_WORLD,
+                         &reqstat);
             }
-            t_end = MPI_Wtime ();
+            t_end = MPI_Wtime();
 
         } else if (myid == 1) {
             for (i = 0; i < loop + skip; i++) {
-                MPI_Recv (r_buf, size, MPI_CHAR, 0, 1, MPI_COMM_WORLD,
-                          &reqstat);
-                MPI_Send (s_buf, size, MPI_CHAR, 0, 1, MPI_COMM_WORLD);
+                MPI_Recv(r_buf, size, MPI_CHAR, 0, 1, MPI_COMM_WORLD,
+                         &reqstat);
+                MPI_Send(s_buf, size, MPI_CHAR, 0, 1, MPI_COMM_WORLD);
             }
         }
 
         if (myid == 0) {
-            double      latency;
+            double latency;
             latency = (t_end - t_start) * 1.0e6 / (2.0 * loop);
-            fprintf (stdout, "%d\t\t%0.2f\n", size, latency);
+            fprintf(stdout, "%d\t\t%0.2f\n", size, latency);
         }
+
     }
 
-    MPI_Finalize ();
+    MPI_Finalize();
     return 0;
+
 }
