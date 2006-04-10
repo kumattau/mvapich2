0332b8e1e57b28a94f4d61ce5ab2f37b044df0bb
huangwei <huangwei@09bc9535-d30e-0410-b1f7-d46b20a4725c>
2006-04-10 21:18:11 +0000

    one sided tests copyright should start from 2003
    git-svn-id: https://scm.mvapich.cse.ohio-state.edu/svn/mpi/mvapich2/trunk@64 09bc9535-d30e-0410-b1f7-d46b20a4725c

--- osu_benchmarks/osu_acc_latency.c
+++ osu_benchmarks/osu_acc_latency.c
@@ -2,7 +2,7 @@
  * OSU MPI One Sided MPI_Accumulate Latency test v2.2
  */
 /*
- * Copyright (C) 2002-2006 the Network-Based Computing Laboratory
+ * Copyright (C) 2003-2006 the Network-Based Computing Laboratory
  * (NBCL), The Ohio State University.
  *
  * Contact: Dr. D. K. Panda (panda@cse.ohio-state.edu)
