cf6980df257022cf90a46591ab5e3ebcf73af14f
huangwei <huangwei@09bc9535-d30e-0410-b1f7-d46b20a4725c>
2006-08-16 19:09:28 +0000

    The assertion is too strong when there is multiple rails -- packets may arrive out of order. Since rma packets is not re-ordered, out of order packets may cause the ch3u_handle_recv_req being recursively called with different paramters.
    git-svn-id: https://scm.mvapich.cse.ohio-state.edu/svn/mpi/mvapich2/trunk@436 09bc9535-d30e-0410-b1f7-d46b20a4725c

--- src/mpid/osu_ch3/src/ch3u_handle_recv_req.c
+++ src/mpid/osu_ch3/src/ch3u_handle_recv_req.c
@@ -45,6 +45,7 @@ int MPIDI_CH3U_Handle_recv_req(MPIDI_VC_t * vc, MPID_Request * rreq,
 			       int *complete)
 {
     static int in_routine = FALSE;
+    MPID_Request *curr_req = NULL;
     MPID_Win *win_ptr;
     int mpi_errno = MPI_SUCCESS;
     MPIDI_STATE_DECL(MPID_STATE_MPIDI_CH3U_HANDLE_RECV_REQ);
@@ -52,7 +53,7 @@ int MPIDI_CH3U_Handle_recv_req(MPIDI_VC_t * vc, MPID_Request * rreq,
 
     MPIDI_FUNC_ENTER(MPID_STATE_MPIDI_CH3U_HANDLE_RECV_REQ);
 
-    MPIU_Assert(in_routine == FALSE);
+    MPIU_Assert(in_routine == FALSE || curr_req != rreq);
     in_routine = TRUE;
 
     switch (rreq->dev.ca) {
