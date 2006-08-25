9e7053137f8fba33130e550e38bea103eb23236d
surs <surs@09bc9535-d30e-0410-b1f7-d46b20a4725c>
2006-08-25 18:02:37 +0000

    -- Include mpimem.h for all macros
    git-svn-id: https://scm.mvapich.cse.ohio-state.edu/svn/mpi/mvapich2/trunk@460 09bc9535-d30e-0410-b1f7-d46b20a4725c

--- src/util/thread/mpe_thread_posix.c
+++ src/util/thread/mpe_thread_posix.c
@@ -6,6 +6,7 @@
 
 #include <stdlib.h>
 #include "mpe_thread.h"
+#include "mpimem.h"
 
 /*
  * struct MPEI_Thread_info
--- src/util/thread/mpe_thread_solaris.c
+++ src/util/thread/mpe_thread_solaris.c
@@ -7,6 +7,7 @@
 
 #include <stdlib.h>
 #include "mpe_thread.h"
+#include "mpimem.h"
 
 
 /*
