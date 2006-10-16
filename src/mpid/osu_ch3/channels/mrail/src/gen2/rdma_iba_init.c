326472c1bd77fefc08eadab9da91611d77a3cb11
gaoq <gaoq@09bc9535-d30e-0410-b1f7-d46b20a4725c>
2006-10-16 23:02:13 +0000

    Apply the change to finalization to CM_Finalize
    git-svn-id: https://scm.mvapich.cse.ohio-state.edu/svn/mpi/mvapich2/trunk@629 09bc9535-d30e-0410-b1f7-d46b20a4725c

--- src/mpid/osu_ch3/channels/mrail/src/gen2/rdma_iba_init.c
+++ src/mpid/osu_ch3/channels/mrail/src/gen2/rdma_iba_init.c
@@ -1046,6 +1046,10 @@ int MPIDI_CH3I_CM_Finalize()
     pg_rank = MPIDI_Process.my_pg_rank;
     pg_size = MPIDI_PG_Get_size(pg);
 
+#ifndef DISABLE_PTMALLOC
+    mvapich2_mfin();
+#endif
+
     /*barrier to make sure queues are initialized before continuing */
     error = PMI_Barrier();
 
@@ -1149,10 +1153,6 @@ int MPIDI_CH3I_CM_Finalize()
         ibv_dealloc_pd(MPIDI_CH3I_RDMA_Process.ptag[i]);
         ibv_close_device(MPIDI_CH3I_RDMA_Process.nic_context[i]);
     }
-
-#ifndef DISABLE_PTMALLOC
-    mvapich2_mfin();
-#endif
 
     return MPI_SUCCESS;
 }
