842b9c733741d042b7bde0710644d6a28314442c
huangwei <huangwei@09bc9535-d30e-0410-b1f7-d46b20a4725c>
2007-07-26 01:33:29 +0000

    License information for shm_coll files
    git-svn-id: https://scm.mvapich.cse.ohio-state.edu/svn/mpi/mvapich2/trunk@1424 09bc9535-d30e-0410-b1f7-d46b20a4725c

--- src/mpi/coll/barrier.c
+++ src/mpi/coll/barrier.c
@@ -5,6 +5,18 @@
  *      See COPYRIGHT in top-level directory.
  */
 
+/* Copyright (c) 2003-2007, The Ohio State University. All rights
+ * reserved.
+ *
+ * This file is part of the MVAPICH2 software package developed by the
+ * team members of The Ohio State University's Network-Based Computing
+ * Laboratory (NBCL), headed by Professor Dhabaleswar K. (DK) Panda.
+ *
+ * For detailed copyright and licensing information, please refer to the
+ * copyright file COPYRIGHT_MVAPICH2 in the top level MVAPICH2 directory.
+ *
+ */
+
 #include "mpiimpl.h"
 
 /* -- Begin Profiling Symbol Block for routine MPI_Barrier */
