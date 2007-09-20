198e58b747eff9f487052c85a1f06cd16b668d72
huangwei <huangwei@09bc9535-d30e-0410-b1f7-d46b20a4725c>
2007-09-20 18:19:24 +0000

    totalview patch
    git-svn-id: https://scm.mvapich.cse.ohio-state.edu/svn/mpi/mvapich2/trunk@1518 09bc9535-d30e-0410-b1f7-d46b20a4725c

--- src/pm/mpd/mtv_setup.py
+++ src/pm/mpd/mtv_setup.py
@@ -6,6 +6,9 @@
 
 from distutils.core import setup, Extension
 
-mtv = Extension("mtv",["mtv.c"])
+mtv = Extension("mtv",
+                sources = ["mtv.c"],
+                extra_compile_args = ["-O0", "-g"],
+                extra_link_args = ["-O0", "-g"])
 
 setup(name="mtv", version="1.0", ext_modules=[mtv])
