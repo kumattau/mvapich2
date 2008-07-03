a464996c6c89f6e2cb1d998a986cc258be5f94c6
curtisbr <curtisbr@09bc9535-d30e-0410-b1f7-d46b20a4725c>
2008-07-03 03:58:34 +0000

    Add copyright to modified files.
    git-svn-id: https://scm.mvapich.cse.ohio-state.edu/svn/mpi/mvapich2/trunk@2826 09bc9535-d30e-0410-b1f7-d46b20a4725c

--- maint/release.pl
+++ maint/release.pl
@@ -1,4 +1,13 @@
 #!/usr/bin/env perl
+# Copyright (c) 2003-2008, The Ohio State University. All rights
+# reserved.
+#
+# This file is part of the MVAPICH2 software package developed by the
+# team members of The Ohio State University's Network-Based Computing
+# Laboratory (NBCL), headed by Professor Dhabaleswar K. (DK) Panda.
+#
+# For detailed copyright and licensing information, please refer to the
+# copyright file COPYRIGHT in the top level MVAPICH2 directory.
 
 use strict;
 use warnings;
