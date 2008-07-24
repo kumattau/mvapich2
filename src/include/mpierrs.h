56e43f6d35e30347a30aa39a10a07f6c31bf6084
curtisbr <curtisbr@09bc9535-d30e-0410-b1f7-d46b20a4725c>
2008-07-24 18:46:05 +0000

    Remove copyright now that modifications are not present.
    git-svn-id: https://scm.mvapich.cse.ohio-state.edu/svn/mpi/mvapich2/trunk@2866 09bc9535-d30e-0410-b1f7-d46b20a4725c

--- src/include/mpierrs.h
+++ src/include/mpierrs.h
@@ -4,19 +4,6 @@
  *  (C) 2001 by Argonne National Laboratory.
  *      See COPYRIGHT in top-level directory.
  */
-
-/* Copyright (c) 2003-2008, The Ohio State University. All rights
- * reserved.
- *
- * This file is part of the MVAPICH2 software package developed by the
- * team members of The Ohio State University's Network-Based Computing
- * Laboratory (NBCL), headed by Professor Dhabaleswar K. (DK) Panda.
- *
- * For detailed copyright and licensing information, please refer to the
- * copyright file COPYRIGHT in the top level MVAPICH2 directory.
- *
- */
-
 #ifndef MPIERRS_H_INCLUDED
 #define MPIERRS_H_INCLUDED
 /* ------------------------------------------------------------------------- */
