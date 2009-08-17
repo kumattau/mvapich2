c8e860f74ed373d8554505fe52795c5f984c8000
subramon <subramon@09bc9535-d30e-0410-b1f7-d46b20a4725c>
2009-08-17 16:27:40 +0000

    Fix for Trac #435 - Issue #1
    git-svn-id: https://scm.mvapich.cse.ohio-state.edu/svn/mpi/mvapich2/trunk@3452 09bc9535-d30e-0410-b1f7-d46b20a4725c

--- src/mpid/ch3/channels/mrail/src/gen2/rdma_iba_priv.c
+++ src/mpid/ch3/channels/mrail/src/gen2/rdma_iba_priv.c
@@ -1590,7 +1590,7 @@ int get_pkey_index(uint16_t pkey, int hca_num, int port_num, uint16_t* index)
         uint16_t curr_pkey;
         ibv_query_pkey(MPIDI_CH3I_RDMA_Process.nic_context[hca_num], 
                 (uint8_t)port_num, (int)i ,&curr_pkey);
-        if (pkey == ntohs(curr_pkey) & PKEY_MASK) {
+        if (pkey == (ntohs(curr_pkey) & PKEY_MASK)) {
             *index = i;
             return 1;
         }
