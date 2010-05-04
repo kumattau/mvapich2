57a0f5babda91234837300dac8da0a9a2abd681c
perkinjo <perkinjo@09bc9535-d30e-0410-b1f7-d46b20a4725c>
2010-05-04 21:32:34 +0000

    Resolve unmerged items
    git-svn-id: https://scm.mvapich.cse.ohio-state.edu/svn/mpi/mvapich2/trunk@3865 09bc9535-d30e-0410-b1f7-d46b20a4725c

--- maint/release.pl
+++ maint/release.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/env perl
-# Copyright (c) 2003-2009, The Ohio State University. All rights
+# Copyright (c) 2003-2010, The Ohio State University. All rights
 # reserved.
 #
 # This file is part of the MVAPICH2 software package developed by the
@@ -8,6 +8,21 @@
 #
 # For detailed copyright and licensing information, please refer to the
 # copyright file COPYRIGHT in the top level MVAPICH2 directory.
+#
+# (C) 2008 by Argonne National Laboratory.
+#     See COPYRIGHT in top-level directory.
+#
+#
+# Known limitations:
+#
+#    1. This script assumes that it is the only client accessing the
+#    svn server. Version number verifications, diffs for ABI
+#    mismatches and other checks are run assuming atomicity.
+#
+#    2. ABI mismatch checks are run using an svn diff in mpi.h.in and
+#    the binding directory. This can come up with false positives, and
+#    is only meant to be a worst-case guess.
+#
 
 use strict;
 use warnings;
@@ -17,9 +32,15 @@ use Getopt::Long;
 
 my $arg = 0;
 my $source = "";
+my $psource = "";
 my $version = "";
-my $pack = "";
+my $append_svnrev;
 my $root = $ENV{PWD};
+my $with_autoconf = "";
+my $with_automake = "";
+
+# Default to MPICH2
+my $pack = "mpich2";
 
 my $logfile = "release.log";
 
@@ -27,8 +48,27 @@ sub usage
 {
 # <_OSU_MVAPICH_>
 #    print "Usage: $0 [--source source] {--package package} [version]\n";
-    print "Usage: $0 [--source source] [version]\n";
+    print "Usage: $0 [OPTIONS] [--source source] [version]\n";
 # </_OSU_MVAPICH_>
+    print "OPTIONS:\n";
+
+    # Source svn repository from where the package needs to be downloaded from
+    print "\t--source          source svn repository (required)\n";
+
+    # svn repository for the previous source in this series to ensure ABI compliance
+    print "\t--psource    source repo for the previous version for ABI compliance (required)\n";
+
+    # what package we are creating
+    print "\t--package         package to create (optional)\n";
+
+    # version string associated with the tarball
+    print "\t--version         tarball version (required)\n";
+
+    # append svn revision
+    print "\t--append-svnrev   append svn revision number (optional)\n";
+
+    print "\n";
+
     exit;
 }
 
@@ -37,9 +77,25 @@ sub check_package
     my $pack = shift;
 
     print "===> Checking for package $pack... ";
-    if (`which $pack` eq "") {
-	print "not found\n";
-	exit;
+    if ($with_autoconf and ($pack eq "autoconf")) {
+        # the user specified a dir where autoconf can be found
+        if (not -x "$with_autoconf/$pack") {
+            print "not found\n";
+            exit;
+        }
+    }
+    if ($with_automake and ($pack eq "automake")) {
+        # the user specified a dir where automake can be found
+        if (not -x "$with_automake/$pack") {
+            print "not found\n";
+            exit;
+        }
+    }
+    else {
+        if (`which $pack` eq "") {
+            print "not found\n";
+            exit;
+        }
     }
     print "done\n";
 }
@@ -48,9 +104,8 @@ sub run_cmd
 {
     my $cmd = shift;
 
-    # FIXME: Allow for verbose output; just have to remove the
-    # redirection to /dev/null
-    system("$cmd 2>&1 | tee -a $root/$logfile > /dev/null");
+    # FIXME: Allow for verbose output
+    system("$cmd >> $root/$logfile 2>&1");
     if ($?) {
         die "unable to execute ($cmd), \$?=$?.  Stopped";
     }
@@ -260,10 +315,17 @@ GetOptions(
 # <_OSU_MVAPICH_>
 #    "package:s"  => \$pack,
 # </_OSU_MVAPICH_>
+    "source=s" => \$source,
+    "psource=s" => \$psource,
+    "package:s"  => \$pack,
+    "version=s" => \$version,
+    "append-svnrev!" => \$append_svnrev,
+    "with-autoconf" => \$with_autoconf,
+    "with-automake" => \$with_automake,
     "help"     => \&usage,
 ) or die "unable to parse options, stopped";
 
-if (scalar(@ARGV) != 1) {
+if (scalar(@ARGV) != 0) {
     usage();
 }
 
@@ -275,20 +337,21 @@ $version = $ARGV[0];
 #}
 # </_OSU_MVAPICH_>
 
-if (!$source || !$version) {
+if (!$source || !$version || !$psource) {
     usage();
 }
 
 check_package("doctext");
+check_package("txt2man");
 check_package("svn");
 check_package("latex");
 check_package("autoconf");
-debug "\n";
+check_package("automake");
+print("\n");
 
-my $current_ver = `svn cat ${source}/maint/Version`;
-
+my $current_ver = `svn cat ${source}/maint/Version | grep ^MPICH2_VERSION: | cut -f2 -d' '`;
 if ("$current_ver" ne "$version\n") {
-    debug("\n\tWARNING: Version mismatch\n\n");
+    print("\tWARNING: Version mismatch\n\n");
 }
 
 system("rm -f ${root}/$logfile");
@@ -311,3 +374,110 @@ system("rm -f ${root}/$logfile");
 #    die "Unknown package: $pack";
 #}
 # </_OSU_MVAPICH_>
+
+if ($psource) {
+    # Check diff
+    my $d = `svn diff ${psource}/src/include/mpi.h.in ${source}/src/include/mpi.h.in`;
+    $d .= `svn diff ${psource}/src/binding ${source}/src/binding`;
+    if ("$d" ne "") {
+	print("\tWARNING: ABI mismatch\n\n");
+    }
+}
+
+if ($append_svnrev) {
+    $version .= "-r";
+    $version .= `svn info ${source} | grep ^Revision: | cut -f2 -d' ' | xargs echo -n`;
+}
+
+# Clean up the log file
+system("rm -f ${root}/$logfile");
+
+# Check out the appropriate source
+print("===> Checking out $pack SVN source... ");
+run_cmd("rm -rf ${pack}-${version}");
+run_cmd("svn export -q ${source} ${pack}-${version}");
+print("done\n");
+
+# Remove packages that are not being released
+print("===> Removing packages that are not being released... ");
+chdir("${root}/${pack}-${version}");
+run_cmd("rm -rf src/mpid/globus doc/notes src/pm/mpd/Zeroconf.py");
+
+chdir("${root}/${pack}-${version}/src/mpid/ch3/channels/nemesis/nemesis/netmod");
+my @nem_modules = qw(elan ib psm);
+run_cmd("rm -rf ".join(' ', map({$_ . "/*"} @nem_modules)));
+for my $module (@nem_modules) {
+    # system to avoid problems with shell redirect in run_cmd
+    system(qq(echo "# Stub Makefile" > ${module}/Makefile.sm));
+}
+print("done\n");
+
+# Create configure
+print("===> Creating configure in the main package... ");
+chdir("${root}/${pack}-${version}");
+{
+    my $cmd = "./maint/updatefiles";
+    $cmd .= " --with-autoconf=$with_autoconf" if $with_autoconf;
+    $cmd .= " --with-automake=$with_automake" if $with_automake;
+    run_cmd($cmd);
+}
+print("done\n");
+
+# Remove unnecessary files
+print("===> Removing unnecessary files in the main package... ");
+chdir("${root}/${pack}-${version}");
+run_cmd("rm -rf README.vin maint/config.log maint/config.status unusederr.txt src/mpe2/src/slog2sdk/doc/jumpshot-4/tex");
+run_cmd("find . -name autom4te.cache | xargs rm -rf");
+print("done\n");
+
+# Get docs
+print("===> Creating secondary package for the docs... ");
+chdir("${root}");
+run_cmd("cp -a ${pack}-${version} ${pack}-${version}-tmp");
+print("done\n");
+
+print("===> Configuring and making the secondary package... ");
+chdir("${root}/${pack}-${version}-tmp");
+{
+    my $cmd = "./maint/updatefiles";
+    $cmd .= " --with-autoconf=$with_autoconf" if $with_autoconf;
+    $cmd .= " --with-automake=$with_automake" if $with_automake;
+    run_cmd($cmd);
+}
+run_cmd("./configure --disable-mpe --disable-f90 --disable-f77 --disable-cxx");
+run_cmd("(make mandoc && make htmldoc && make latexdoc)");
+print("done\n");
+
+print("===> Copying docs over... ");
+chdir("${root}/${pack}-${version}-tmp");
+run_cmd("cp -a man ${root}/${pack}-${version}");
+run_cmd("cp -a www ${root}/${pack}-${version}");
+run_cmd("cp -a doc/userguide/user.pdf ${root}/${pack}-${version}/doc/userguide");
+run_cmd("cp -a doc/installguide/install.pdf ${root}/${pack}-${version}/doc/installguide");
+run_cmd("cp -a doc/smpd/smpd_pmi.pdf ${root}/${pack}-${version}/doc/smpd");
+run_cmd("cp -a doc/logging/logging.pdf ${root}/${pack}-${version}/doc/logging");
+run_cmd("cp -a doc/windev/windev.pdf ${root}/${pack}-${version}/doc/windev");
+chdir("${root}");
+run_cmd("rm -rf ${pack}-${version}-tmp");
+print("done\n");
+
+print("===> Creating ROMIO docs... ");
+chdir("${root}/${pack}-${version}/src/mpi");
+chdir("romio/doc");
+run_cmd("make");
+run_cmd("rm -f users-guide.blg users-guide.toc users-guide.aux users-guide.bbl users-guide.log users-guide.dvi");
+print("done\n");
+
+print( "===> Creating MPE docs... ");
+chdir("${root}/${pack}-${version}/src");
+chdir("mpe2/maint");
+run_cmd("make -f Makefile4man");
+print("done\n");
+
+# Create the tarball
+print("===> Creating the final ${pack} tarball... ");
+chdir("${root}");
+run_cmd("tar -czvf ${pack}-${version}.tar.gz ${pack}-${version}");
+run_cmd("rm -rf ${pack}-${version}");
+print("done\n\n");
+
