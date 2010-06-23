931906b7611a1c46c748e24adf1c80c8d6919d3c
mancini <mancini@09bc9535-d30e-0410-b1f7-d46b20a4725c>
2010-06-23 15:07:45 +0000

    Updated copyrights
    git-svn-id: https://scm.mvapich.cse.ohio-state.edu/svn/mpi/mvapich2/trunk@3971 09bc9535-d30e-0410-b1f7-d46b20a4725c

--- src/mpid/ch3/channels/mrail/src/gen2/cr.c
+++ src/mpid/ch3/channels/mrail/src/gen2/cr.c
@@ -1,14 +1,15 @@
-/* Copyright (c) 2002-2010, The Ohio State University. All rights
+/* Copyright (c) 2003-2010, The Ohio State University. All rights
  * reserved.
  *
- * This file is part of the MVAPICH software package developed by the
+ * This file is part of the MVAPICH2 software package developed by the
  * team members of The Ohio State University's Network-Based Computing
  * Laboratory (NBCL), headed by Professor Dhabaleswar K. (DK) Panda.
  *
  * For detailed copyright and licensing information, please refer to the
- * copyright file COPYRIGHT_MVAPICH in the top level MPICH directory.
+ * copyright file COPYRIGHT in the top level MVAPICH2 directory.
  *
  */
+
 /*
  *
  *  (C) 2001 by Argonne National Laboratory.
